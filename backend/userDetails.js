697f53b1caf57eda5704b87f00ff346f32954e6a
Ilhem Afli <82854562+Ilhem331@users.noreply.github.com>
2023-09-26 15:54:41 +0100

    Update userDetails.js

--- backend/userDetails.js
+++ backend/userDetails.js
@@ -3,6 +3,7 @@ const userSchema = new mongoose.Schema({
 username: { type: String },
 email: { type: String, required: true, unique: true },
 password: { type: String, required: true, minlength: 5 },
+verified: { type: Boolean, default: false },
 pic: {
     type: String,
     default:
